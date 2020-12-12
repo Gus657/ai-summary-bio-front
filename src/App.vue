3c7c4018b808e5c81d9ddb07a7ef3100dbc5b06a
Gustavo Perez <ggusperezg@gmail.com>
2020-12-11 20:24:44 -0600

    Initial Documents

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App'
+
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+  margin-left: 30px;
+  margin-right: 30px;
+}
+</style>
