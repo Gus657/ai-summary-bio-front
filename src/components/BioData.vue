a5c0141ad2dee6b0cc9fbad1892e6ee79328a461
Gustavo Perez <ggusperezg@gmail.com>
2021-01-20 15:51:44 -0600

    Fix Adding a missing parenthesis
    A missing closing parenthesis was added at applicants reviewed text.

--- src/components/BioData.vue
+++ src/components/BioData.vue
@@ -15,7 +15,7 @@
                <div class="summary md-layout-item">
                  <md-card-header>
                       <div class="md-title">
-                          Summary of Bio
+                          Summary of Bio - Genome Desc.
                 </div>
                      <p class="md-subhead separator">  <i>{{peopleData.summary}}</i></p>  
                    <div class="md-title">
